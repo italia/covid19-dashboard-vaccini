df258a6248739274631fdd89b10c0e701bad622a
Alessandro Sebastiani <sebbalex@gmail.com>
2021-01-03 17:35:49 +0100

    chore: fix banner style, ignore eslintcache, include first dataset

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -10,27 +10,29 @@ export const HeaderBar = () => {
   return (
     <>
       <Header small={false} theme="" type="slim" className="App-header">
-          <div className="d-flex justify-content-center mb-3 logo-section mx-auto">
-            <div className="presidenza">
-              <img src="logo.svg" alt="Logo" className="logo" />
-              <span>Presidenza Del</span>
-              <span>Consiglio dei Ministri</span>
-            </div>
-            <div className="presidenza">
-              <img src="logo.svg" alt="Logo" className="logo" />
-              <span>Commissario</span>
-              <span>Straordinario Covid-19</span>
-            </div>
-            <div className="presidenza">
-              <img src="logo.svg" alt="Logo" className="logo" />
-              <span>Ministero</span>
-              <span>della Salute</span>
-            </div>
+        <div className="d-flex justify-content-center mb-3 logo-section mx-auto">
+          <div className="presidenza">
+            <img src="logo.svg" alt="Logo" className="logo" />
+            <span>Presidenza Del</span>
+            <span>Consiglio dei Ministri</span>
           </div>
+          <div className="presidenza">
+            <img src="logo.svg" alt="Logo" className="logo" />
+            <span>Commissario</span>
+            <span>Straordinario Covid-19</span>
+          </div>
+          <div className="presidenza">
+            <img src="logo.svg" alt="Logo" className="logo" />
+            <span>Ministero</span>
+            <span>della Salute</span>
+          </div>
+        </div>
       </Header>
-      <div className="d-flex justify-content-center mainBanner">
-        <img src="logo.png" alt="Logo" className="main-logo" />
-        <span>Report Vaccini Anti COVID-19</span>
+      <div className="d-flex flex-column justify-content-center mainBanner">
+        <div className="d-flex justify-content-center">
+          <img src="logo.png" alt="Logo" className="main-logo" />
+          <span>Report Vaccini Anti COVID-19</span>
+        </div>
       </div>
     </>
   );
