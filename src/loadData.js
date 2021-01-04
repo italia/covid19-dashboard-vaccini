2af9f1373960f037b492f72cb14adb5a36fe0ee5
Alessandro Sebastiani <sebbalex@gmail.com>
2021-01-04 20:02:28 +0100

    chore: new data

--- src/loadData.js
+++ src/loadData.js
@@ -2,15 +2,16 @@ import { sumDoseX, filterByAreaITA, replaceArea } from "./utils";
 
 const sommVaxSummaryURL = "data/somministrazioni-vaccini-summary-latest.json";
 const sommVaxDetailURL = "data/somministrazioni-vaccini-latest.json";
-const consegneVaxSummaryURL = "data/consegne-vaccini-summary-latest.json";
+const deliveryVaxDetailURL = "data/consegne-vaccini-latest.json";
+const vaxSummaryURL = "data/vaccini-summary-latest.json";
 
 const elaborate = (data) => {
   console.log(data);
-  const timestamp = data.dataConsegneVaxSummary.data.slice(0, 1)[0].data;
+  const timestamp = data.dataDeliveryVaxDetail.data.slice(0, 1)[0].data;
   const tot = data.dataSommVaxSummary.data
     .filter(filterByAreaITA)
     .reduce(sumDoseX("totale"), 0);
-  const deliverySummary = data.dataConsegneVaxSummary.data.map(replaceArea);
+  const deliverySummary = data.dataVaxSummary.data.map(replaceArea);
   //   //   const tot2 = data.reduce(sumDoseXY("TML_DOSE_1", "TML_DOSE_2"), 0);
   //   const areasRAW = data.reduce(aggrBy("TML_AREA"), {});
   //   const tot_m = data.reduce(sumDoseX("TML_SESSO_M"), 0);
@@ -37,15 +38,18 @@ const elaborate = (data) => {
 export const loadData = async () => {
   const resSommVaxSummary = await fetch(sommVaxSummaryURL);
   const resSommVaxDetail = await fetch(sommVaxDetailURL);
-  const resConsegneVaxSummary = await fetch(consegneVaxSummaryURL);
+  const resDeliveryVaxDetail = await fetch(deliveryVaxDetailURL);
+  const resVaxSummary = await fetch(vaxSummaryURL);
 
   const dataSommVaxSummary = await resSommVaxSummary.json();
   const dataSommVaxDetail = await resSommVaxDetail.json();
-  const dataConsegneVaxSummary = await resConsegneVaxSummary.json();
+  const dataDeliveryVaxDetail = await resDeliveryVaxDetail.json();
+  const dataVaxSummary = await resVaxSummary.json();
 
   return elaborate({
     dataSommVaxSummary,
     dataSommVaxDetail,
-    dataConsegneVaxSummary,
+    dataDeliveryVaxDetail,
+    dataVaxSummary,
   });
 };
